36e4cf2eb3fc5e246a58e81782c31263db6ef038
fabio henrique <henri8274@gmail.com>
2025-02-27 20:00:58 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,14 @@ function App() {
     <>
     <header>
       <h1>Delícia viral</h1>
+      <p>CLIQUE NA RECEITA PARA VER</p>
     </header>
     <div className='Links'>
-      <a href='#'>Link1</a>
-      <a href='#'>Link1</a>
-      <a href='#'>Link1</a>
-      <a href='#'>Link1</a>
-      <a href='#'>Link1</a>
+      <a href='https://blog-lemon-eight-12.vercel.app/Receita9'>Sanduíche de Presunto e Queijo</a>
+      <a href='https://blog-lemon-eight-12.vercel.app/Receita1'>Panqueca de Maisena</a>
+      <a href='https://blog-lemon-eight-12.vercel.app/Receita2'>Fricassê de frango</a>
+      <a href='#'>Outros</a>
+      <a href='#'>Outros</a>
     </div>
     <footer>
       <h3>&copy; Todos os direitos reservados</h3>
