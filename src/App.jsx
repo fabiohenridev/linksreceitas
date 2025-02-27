7fd048689e59987def8662c31f6dbe7cc8786662
fabio henrique <henri8274@gmail.com>
2025-02-27 19:45:05 -0300

    receitas

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <header>
+      <h1>Delícia viral</h1>
+    </header>
+    <div className='Links'>
+      <a href='#'>Link1</a>
+      <a href='#'>Link1</a>
+      <a href='#'>Link1</a>
+      <a href='#'>Link1</a>
+      <a href='#'>Link1</a>
+    </div>
+    <footer>
+      <h3>&copy; Todos os direitos reservados</h3>
+    </footer>
+    </>
+  )
+}
+
+export default App
